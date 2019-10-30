490e96b973f9a8988acabe279a86c33af088f361
Carlos Encinas <carlos-encinas@hotmail.com>
2019-10-30 09:49:52 -0700

    i

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,38 @@
 const canvas = document.getElementById('glcanvas');
 
+const vsSource = `
+  attribute vec4 aVertexPosition;
+
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }
+`;
+
+const fsSource = `
+  void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  }
+`;
+
+const loadShader = (gl, type , source)=>{
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader , source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+  return shader;
+}
+const initShader = (gl , vsShader , fsShader) =>{
+  
+}
+
 const main = ()=>{
 
     const gl = canvas.getContext('webgl');
@@ -8,7 +41,7 @@ const main = ()=>{
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
       }
-
+    initShader(gl , vsSource , fsSource)
     gl.clearColor(0 , 0 , 0 , 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
